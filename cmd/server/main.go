4e0d232c9a289aa39fc9e5f6c91af7016efc7845
Wander Maia <wander.silva@semeartech.com.br>
2024-04-27 20:34:12 -0300

    Ajustatado o handler para a consulta no ViaCep

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,85 +1,24 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
 	"net/http"
+
+	"github.com/wandermaia/desafio-multithreading/internal/infra/webserver/handlers"
 )
 
-type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
-}
-
 func main() {
-	http.HandleFunc("/", BuscaCepHandler)
+	http.HandleFunc("/", handlers.BuscaCepHandler)
 	http.ListenAndServe(":8000", nil)
-}
-
-func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	/*
-	   Valida o parâmetro CEP que vem na query da URL.
-	   http://localhost:8000/?cep=32450000
-	*/
-
-	cepParam := r.URL.Query().Get("cep")
-	if cepParam == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	cep, err := BuscaCep(cepParam)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(cep)
-
-}
-
-func BuscaCep(cep string) (*ViaCEP, error) {
-	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
-	if error != nil {
-		return nil, error
-	}
-	defer resp.Body.Close()
-	body, error := io.ReadAll(resp.Body)
-	if error != nil {
-		return nil, error
-	}
-
-	var c ViaCEP
-	error = json.Unmarshal(body, &c)
-
-	if error != nil {
-		return nil, error
-	}
-
-	return &c, nil
-
 }
 
 /*
 
 go mod init github.com/wandermaia/desafio-multithreading
 go mod tidy
+
+
+Valida o parâmetro CEP que vem na query da URL.
+http://localhost:8000/?cep=32450000
+
 
 */
---
+++ internal/infra/webserver/handlers/cep_handlers.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cep)
+
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := io.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+
+	var c ViaCEP
+	error = json.Unmarshal(body, &c)
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &c, nil
+
+}
