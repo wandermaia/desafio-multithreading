52b80c7144f23f71613b130dda691e5eb1e8799d
Wander Maia <wander.silva@semeartech.com.br>
2024-04-27 19:01:55 -0300

    Ajustando arquivos

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,16 +6,13 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/wandermaia/desafio-multithreading/internal/infra/webserver/handlers"
 )
 
 func main() {
 
-	cepHandler := handlers.NewCepHandler()
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
-	router.Get("/{cep}", cepHandler.GetCep)
 	log.Println("Servidor iniciado!")
 	http.ListenAndServe(":8000", router)
 }
