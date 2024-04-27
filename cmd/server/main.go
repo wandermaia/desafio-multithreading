b2f93fa9900de3a095866e59f133727d37558287
Wander Maia <wander.silva@semeartech.com.br>
2024-04-27 19:34:44 -0300

    Ajuste

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,80 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"io"
 	"net/http"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
-func main() {
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	log.Println("Servidor iniciado!")
-	http.ListenAndServe(":8000", router)
+func main() {
+	http.HandleFunc("/", BuscaCepHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	/*
+	   Valida o parâmetro CEP que vem na query da URL.
+	   http://localhost:8000/?cep=32450000
+	*/
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cep)
+
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := io.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+
+	var c ViaCEP
+	error = json.Unmarshal(body, &c)
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &c, nil
+
 }
 
 /*
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/wandermaia/desafio-multithreading
 
 go 1.22.1
-
-require github.com/go-chi/chi v1.5.5
